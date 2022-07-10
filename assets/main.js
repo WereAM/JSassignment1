0ae34dafc706744409402d1350b1394041efd305
WereAM <michelle.ambunya@strathmore.edu>
2022-07-10 22:20:16 +0300

    commit

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ let yourname = document.querySelector("#yourname");
 let output = document.querySelector("#output");
  
 function displayName() {
-    output.innerHTML = "Hello" + yourname.value + ", welcome to the bootcamp";
+    output.innerHTML = "Hello " + yourname.value + ", welcome to the bootcamp";
 }
 
 
